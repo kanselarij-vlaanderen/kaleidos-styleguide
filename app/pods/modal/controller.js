5ef07aa6319845ba1a6577cd887a260db3785dd3
Karel Persoons <karel@brenner.company>
2021-05-25 11:53:03 +0200

    Deployed 6074992

--- app/pods/modal/controller.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-import { action } from '@ember/object';
-export default class ModalController extends Controller {
-  @action
-  onClick() {
-
-  }
-}
--- app/pods/pagination/route.js
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class PaginationRoute extends Route {
-  model() {
-    return null;
-  }
-}
